1781fffdd8eb20672614817b345e80dba9fe9bbf
Mike <mike.fury315@gmail.com>
2022-05-03 14:58:49 +0700

    Updatin

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,4 +1,13 @@
-$('a[href^="#"]').click(function (){
+$('a[href^="#"]').click(function () {
     let valHref = $(this).attr("href");
-    $('html,body').animate({scrollTop : $(valHref).offset().top -50 + "px"});
+    $('html,body').animate({scrollTop: $(valHref).offset().top - 50 + "px"});
+});
+$('#blue').click(function () {
+    $('img[id="t-shirt"]').attr("src", "img/tishkaB.jpg");
+});
+$('#grey').click(function () {
+    $('img[id="t-shirt"]').attr("src", "img/tishka.png");
+});
+$('#yellow').click(function () {
+    $('img[id="t-shirt"]').attr("src", "img/tishkaY.jpg");
 });
